43c3faa30ffa588bb4dcebe2d96d261c8fbf40c3
Shivam000189 <shivamsharmass9897@gmail.com>
2026-06-20 20:02:53 +0530

    Vertex-setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Plum-clone ---- inspired from framer
+
+Vertex ----- inspired from dribble
---
+++ vertex/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='bg-gray-400 opacity-80 h-screen '>
+          
+    </div>
+  )
+}
+
+export default App
